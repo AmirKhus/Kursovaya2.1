968e54a4d8b8559064597cc0149d46bfd1105ba2
AmirKhus <70978948+AmirKhus@users.noreply.github.com>
2020-12-07 10:55:17 +0300

    Каталог товаров

--- src/sample/CatalogController.java
+++ src/sample/CatalogController.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,9 +10,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -39,6 +44,27 @@ public class CatalogController extends loginFail implements Initializable {
     @FXML
     private MenuItem Посмотреть;
 
+    final ObservableList<Product> productData = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Product> catalogTabel;
+
+    @FXML
+    private TableColumn<Product, ImageView> ImageProduct;
+
+    @FXML
+    private TableColumn<Product, String> AftorProduct;
+
+    @FXML
+    private TableColumn<Product, String> SumProduct;
+
+    @FXML
+    private TableColumn<Product, String> idProduct;
+
+    @FXML
+    private TableColumn<Product, String> NameProduct;
+
+
     @FXML
     private Menu Товары;
 
@@ -56,19 +82,87 @@ public class CatalogController extends loginFail implements Initializable {
     private Button MyOrders;
 
     @FXML
-    private void handleShowMyProduct() {
-//        try (Scanner scan = new Scanner(new File("C:\\Users\\KP\\IdeaProjects\\Kursovaya2\\src\\sample\\ProductDataBase.txt"))) {
-//            while(scan.hasNextLine()) {
-//                String[] logon = scan.nextLine().split(",");
-//                Image img = new Image(getClass().getResource("C:\\Users\\KP\\IdeaProjects\\untitled3\\src\\sample\\ruki2.png").toExternalForm());
-//                ImageView photo = new ImageView(img);
-//                new Product(photo,logon[1],logon[2],logon[3],logon[4]);
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//        MyProductController myProductController = new MyProductController();
-//        myProductController.ShowInfoProduct();
+    private void handleShowMyProductFavorites() {
+        Stage stage = new Stage();
+        stage.setTitle("Избранные");
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MyProductFavorites.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void BuyProduct() {
+        Product selectedProduct = catalogTabel.getSelectionModel().getSelectedItem();
+        if (selectedProduct != null) {
+            boolean okClicked = showProdoctEditDialog(selectedProduct);
+            if (okClicked) {
+                int selectedIndex = catalogTabel.getSelectionModel().getSelectedIndex();
+                productData.set(selectedIndex, selectedProduct);
+            }
+
+        }else{
+//        Ничего не выбрано.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(null);
+            alert.setTitle("Ничего не набрано");
+            alert.setHeaderText("Нет продукта");
+            alert.setContentText("Выберите продукт в таблице");
+            alert.showAndWait();
+        }
+    }
+
+    public boolean showProdoctEditDialog(Product product){
+        try {
+//          Загружаем fxml-файл и создаем новую сцену для
+//          для всплывающего диалогового окна
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(EditSceneController.class.getResource("productDescription.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+//    Создаем диалоговое окно Stage
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Product");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(null);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+//    Передаём адресата в контроллер.
+            productDescriptionController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setProduct(product);
+// диалоговое окно и ждет, пока пользователь его не закроет
+            dialogStage.showAndWait();
+            return controller.isOkKlicked();
+        }catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void ShowProduct() {
+        try (Scanner scan = new Scanner(new File("C:\\Users\\KP\\IdeaProjects\\Kursovaya2\\src\\sample\\ProductDataBase.txt"))) {
+            while(scan.hasNextLine() ) {
+                String[] logon = scan.nextLine().split(",");
+                System.out.println(logon[0]);
+                Image img = new Image(new FileInputStream(logon[0]));
+                ImageView photo = new ImageView(img);
+                productData.add(new Product(photo, logon[1], logon[2], logon[3], logon[4], Main.user,logon[0],logon[6],logon[7]));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        ImageProduct.setCellValueFactory(data -> data.getValue().productImageProperty());
+        AftorProduct.setCellValueFactory(data -> data.getValue().productAftorProperty());
+        SumProduct.setCellValueFactory(data -> data.getValue().productSumProperty());
+        idProduct.setCellValueFactory(data -> data.getValue().productIdProperty());
+        NameProduct.setCellValueFactory(data -> data.getValue().productNameProperty());
+        catalogTabel.setItems(productData);
     }
 
     @Override
@@ -87,10 +181,8 @@ public class CatalogController extends loginFail implements Initializable {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-
-//            Stage stage1 = (Stage) lodinButton.getScene().getWindow();
-//            stage1.close();
         });
+        ShowProduct();
     }
 
 }
